d5f6a0c0b7478d9fcf03e8f532f232c7ec0698c3
Brent Shaffer <betterbrent@google.com>
2020-09-08 10:41:16 -0700

    feat: use jar for protoc

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -44,7 +44,7 @@ protobuf {
             artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
         }
         id("grpckt") {
-            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion"
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:$grpcKotlinVersion:jdk8@jar"
         }
     }
     generateProtoTasks {
