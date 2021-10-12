b3a522fed27dec6105756b45c79bc5253fbf7637
James Ward <james@jamesward.com>
2021-10-12 13:04:18 -0600

    bump example deps

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     id("com.android.application") version "4.1.1" apply false
-    id("com.google.protobuf") version "0.8.15" apply false
+    id("com.google.protobuf") version "0.8.17" apply false
     kotlin("jvm") version "1.4.32" apply false
     id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
 }
 
 // todo: move to subprojects, but how?
-ext["grpcVersion"] = "1.37.0"
+ext["grpcVersion"] = "1.40.1"
 ext["grpcKotlinVersion"] = "1.1.0" // CURRENT_GRPC_KOTLIN_VERSION
-ext["protobufVersion"] = "3.15.8"
+ext["protobufVersion"] = "3.17.3"
 
 allprojects {
     repositories {
