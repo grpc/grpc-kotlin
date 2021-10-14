80af4b654f2f16381e0dbae3ad169d4df683844e
James Ward <james@jamesward.com>
2021-10-14 15:48:32 -0600

    cleanup

--- examples/android/src/main/kotlin/io/grpc/examples/helloworld/MainActivity.kt
+++ examples/android/src/main/kotlin/io/grpc/examples/helloworld/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.grpc.examples.helloworld
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -16,28 +17,25 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import io.grpc.ManagedChannelBuilder
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.asExecutor
-import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import java.io.Closeable
-import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val url = URL(resources.getString(R.string.server_url))
-        val greeterService = GreeterService(url) // todo: better resource open & closing
+        val uri = Uri.parse(resources.getString(R.string.server_url))
+        val greeterService = GreeterService(uri) // todo: better resource open & closing
 
         setContent {
             Surface(color = MaterialTheme.colors.background) {
@@ -47,18 +45,14 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
-class GreeterService(url: URL) : Closeable {
+class GreeterService(uri: Uri) : Closeable {
     val responseState = mutableStateOf("")
 
-    private val scope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
-
     private val channel = let {
-        val port = if (url.port == -1) url.defaultPort else url.port
+        println("Connecting to ${uri.host}:${uri.port}")
 
-        println("Connecting to ${url.host}:$port")
-
-        val builder = ManagedChannelBuilder.forAddress(url.host, port)
-        if (url.protocol == "https") {
+        val builder = ManagedChannelBuilder.forAddress(uri.host, uri.port)
+        if (uri.scheme == "https") {
             builder.useTransportSecurity()
         } else {
             builder.usePlaintext()
@@ -69,7 +63,7 @@ class GreeterService(url: URL) : Closeable {
 
     private val greeter = GreeterGrpcKt.GreeterCoroutineStub(channel)
 
-    fun sayHello(name: String) = scope.launch {
+    suspend fun sayHello(name: String) {
         try {
             val request = helloRequest { this.name = name }
             val response = greeter.sayHello(request)
@@ -82,12 +76,13 @@ class GreeterService(url: URL) : Closeable {
 
     override fun close() {
         channel.shutdownNow()
-        scope.cancel()
     }
 }
 
 @Composable
 fun Greeter(greeterService: GreeterService) {
+
+    val scope = rememberCoroutineScope()
 
     val nameState = remember { mutableStateOf(TextFieldValue()) }
 
@@ -95,7 +90,7 @@ fun Greeter(greeterService: GreeterService) {
         Text(stringResource(R.string.name_hint), modifier = Modifier.padding(top = 10.dp))
         OutlinedTextField(nameState.value, { nameState.value = it })
 
-        Button({ greeterService.sayHello(nameState.value.text) }, Modifier.padding(10.dp)) {
+        Button({ scope.launch { greeterService.sayHello(nameState.value.text) } }, Modifier.padding(10.dp)) {
         Text(stringResource(R.string.send_request))
     }
 
