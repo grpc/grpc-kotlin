63ad23278ca49b2d5720c02f2cffb120c9fcb80b
Zhumazhenis <zhumazhenis@gmail.com>
2025-07-31 01:07:16 +0100

    Add interceptor for propagating gRPC Metadata to coroutineContext.

---
+++ stub/src/main/java/io/grpc/kotlin/MetadataCoroutineContextInterceptor.kt
@@ -0,0 +1,33 @@
+package io.grpc.kotlin
+
+import io.grpc.Metadata
+import io.grpc.ServerCall
+import kotlin.coroutines.CoroutineContext
+
+/**
+ * Propagates gRPC Metadata (HTTP Headers) to coroutineContext.
+ * Attach the interceptor to gRPC Server and then access the Metadata using extractMetadata() function.
+ * Example usage:
+ *
+ * ServerBuilder.forPort(8060)
+ *   .addService(GreeterImpl())
+ *   .intercept(MetadataCoroutineContextInterceptor())
+ *
+ * extractMetadata()
+ */
+class MetadataCoroutineContextInterceptor : CoroutineContextServerInterceptor() {
+    final override fun coroutineContext(call: ServerCall<*, *>, headers: Metadata): CoroutineContext {
+        return MetadataElement(value = headers)
+    }
+}
+
+/**
+ * Used for accessing the Metadata from coroutineContext.
+ * Example usage:
+ *   coroutineContext[MetadataElement]?.value
+ */
+internal data class MetadataElement(val value: Metadata) : CoroutineContext.Element {
+    companion object Key : CoroutineContext.Key<MetadataElement>
+
+    override val key: CoroutineContext.Key<MetadataElement> get() = Key
+}
