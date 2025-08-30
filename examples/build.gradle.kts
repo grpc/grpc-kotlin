188b74a2739b3e882436d4b3d6be0e3d64747056
Gia Thuan Lam <duckladydinh@gmail.com>
2025-08-30 01:18:01 +0000

    Redesign project formatting.

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -4,19 +4,6 @@ plugins {
     alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.ktlint) apply false
-}
-
-subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-
-    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-        filter {
-            exclude {
-                it.file.path.startsWith(project.layout.buildDirectory.get().dir("generated").toString())
-            }
-        }
-    }
 }
 
 tasks.create("assemble").dependsOn(":server:installDist")
