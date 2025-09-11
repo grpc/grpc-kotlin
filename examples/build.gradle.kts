dbe3c597e7101f09def19421e2a2c0ca6c4d747e
Gia Thuan Lam <duckladydinh@gmail.com>
2025-09-11 21:08:40 +0000

    Upgrade compose plugin to 8.13.0.

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.android.library) apply false
-  alias(libs.plugins.protobuf) apply false
+  alias(libs.plugins.compose.compiler) apply false
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.protobuf) apply false
 }
 
 tasks.create("assemble").dependsOn(":server:installDist")
